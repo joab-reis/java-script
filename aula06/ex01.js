27a65b9fd4885fe64c604487a9dc356e5fecd7f2
joab ferreira reis <joab2012-ferreira@hotmail.com>
2023-05-30 11:32:42 -0300

    aula06

---
+++ aula06/ex01.js
@@ -0,0 +1,15 @@
+
+
+let vel = 70.0
+console.log(`a velocidade do seu carro e ${vel} km/h`)
+if (vel > 60) {
+    console.log("você ultrapassou a velocidade permitida. multado!")
+}
+console.log("dirija sempre com segurança")
+
+/*
+    neste exemplo eu usei uma condiçao simples o (if=se) neste caso 
+    eu pedi que se apenas a minha variavel for maior que 60 que execute 
+    o bloco caso contrario nada aconte apenas a minha condiçao sera ignorada.
+     
+*/ 
