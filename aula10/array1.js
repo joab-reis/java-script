c8577f177a85844eb2cf5dbca1367ba1503911fc
joab ferreira reis <joab2012-ferreira@hotmail.com>
2023-06-07 07:50:37 -0300

    Create array1.js

---
+++ aula10/array1.js
@@ -0,0 +1,15 @@
+
+let contando = [2,6,7,8,1,12]
+
+// se eu quiser mostrar os numeros de cada posiçao do meu array:
+
+for(let pos in contando) {
+    console.log(`A posição ${pos} tem o valor: ${contando[pos]}`)
+}
+
+/*
+esta estrutura: for(___in____) {
+    bloco
+}
+e uma estrutura de repetiçao simplificada que usada pra variaveis compostas (array)
+*/
